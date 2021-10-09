e8000e186287b1d9f9f729da6ee07ab0a02b3148
Tito-74 <kipkirui133@gmail.com>
2021-10-09 17:18:31 +0300

    returning value from the model

--- photo/models.py
+++ photo/models.py
@@ -2,23 +2,38 @@ from django.db import models
 
 # Create your models here.
 
-class Location():
+class Location(models.Model):
     location = models.CharField(max_length =30)
-    def__str__(self):
+    
+    def __str__(self):
         return self.location
 
+    # def save_location():
+    #    self.save() 
 
-class Category():
+
+class Category(models.Model):
     category = models.CharField(max_length =30)
-    def__str__(self):
-        return self.category
 
-class Post():
+    def __str__(self):
+        return self.category
+    
+    
+    # def save_category():
+    #    self.save() 
+
+class Post(models.Model):
     name = models.CharField(max_length =60)
     desc = models.TextField()
-    category = models.ForeignKey(category)
-    location = models.ForeignKey(location)
-    def__str__(self):
-        return f'{self.name} {self.desc}'
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    location = models.ForeignKey(Location,on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
     
+
+    # f'{self.name} {self.desc}'
     
+    # def save_post():
+    #    self.save() 
