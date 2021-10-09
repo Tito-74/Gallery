50e9f126da25661b337251a0ff1a5cd1e76e0765
Tito-74 <kipkirui133@gmail.com>
2021-10-09 16:49:52 +0300

    creating models

--- photo/models.py
+++ photo/models.py
@@ -3,14 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Location():
+    location = models.CharField(max_length =30)
     def__str__(self):
-        return self
+        return self.location
 
 
 class Category():
+    category = models.CharField(max_length =30)
     def__str__(self):
-        return self
+        return self.category
 
 class Post():
+    name = models.CharField(max_length =60)
+    desc = models.TextField()
+    category = models.ForeignKey(category)
+    location = models.ForeignKey(location)
     def__str__(self):
-        return self
+        return f'{self.name} {self.desc}'
+    
+    
