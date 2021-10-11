9f21b5cc34c9d5a4b4873f8d5c14342bc7499f6c
Tito-74 <kipkirui133@gmail.com>
2021-10-11 18:30:37 +0300

    adding procfile

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+
+
+cloudinary.config( 
+  cloud_name = "dwofopsjd", 
+  api_key = "711236216548118", 
+  api_secret = "HV-nka5FpTRi1N5QSJm80hOT38U" 
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'photo.apps.PhotoConfig',
     'bootstrap3',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
